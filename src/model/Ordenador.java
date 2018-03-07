399730d2044f15a8008eae9a1d4c5ac842dd9494
William Vega <willial1393@gmail.com>
2018-03-06 19:19:36 -0500

    Modelo, vista, controlador base con hilos

--- src/model/Ordenador.java
+++ src/model/Ordenador.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 /**
@@ -10,5 +5,39 @@ package model;
  * @author william
  */
 public class Ordenador {
+    private double memoria;
+    private double discoDuro;
+    private double nucleos;
+
+    public Ordenador(double memoria, double discoDuro, double nucleos) {
+        this.memoria = memoria;
+        this.discoDuro = discoDuro;
+        this.nucleos = nucleos;
+    }
+
+    public double getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(double memoria) {
+        this.memoria = memoria;
+    }
+
+    public double getDiscoDuro() {
+        return discoDuro;
+    }
+
+    public void setDiscoDuro(double discoDuro) {
+        this.discoDuro = discoDuro;
+    }
+
+    public double getNucleos() {
+        return nucleos;
+    }
+
+    public void setNucleos(double nucleos) {
+        this.nucleos = nucleos;
+    }
+    
     
 }
--- src/model/Proceso.java
+++ src/model/Proceso.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 /**
@@ -10,5 +5,81 @@ package model;
  * @author william
  */
 public class Proceso {
-    
+
+    private String nombre;
+    private double memoria;
+    private double discoDuro;
+    private double nucleos;
+    private long tiempo;
+    /**
+     * 1- Inicio
+     * 2- Listo
+     * 3- Ejecución
+     * 4- Espera
+     * 5- Terminado
+     */
+    private int estado;
+
+    public Proceso(String nombre, double memoria, double discoDuro, double nucleos, long tiempo) {
+        this.nombre = nombre;
+        this.memoria = memoria;
+        this.discoDuro = discoDuro;
+        this.nucleos = nucleos;
+        this.tiempo = tiempo;
+        this.estado = 1;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(double memoria) {
+        this.memoria = memoria;
+    }
+
+    public double getDiscoDuro() {
+        return discoDuro;
+    }
+
+    public void setDiscoDuro(double discoDuro) {
+        this.discoDuro = discoDuro;
+    }
+
+    public double getNucleos() {
+        return nucleos;
+    }
+
+    public void setNucleos(double nucleos) {
+        this.nucleos = nucleos;
+    }
+
+    public long getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(long tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + "-" + tiempo + "-" + memoria + "-" + discoDuro + "-" + nucleos;
+    }
+
 }
